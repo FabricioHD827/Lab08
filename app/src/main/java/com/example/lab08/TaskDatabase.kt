c729e2153a039b955cba6d4621fbedba10423399
FabricioHD827 <168231311+FabricioHD827@users.noreply.github.com>
2024-09-23 18:27:35 -0500

    Modificando MainActivity.kt

--- app/src/main/java/com/example/lab08/TaskDatabase.kt
+++ app/src/main/java/com/example/lab08/TaskDatabase.kt
@@ -1,4 +1,9 @@
 package com.example.lab08
+import androidx.room.Database
+import androidx.room.RoomDatabase
 
-class TaskDatabase {
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
 }
